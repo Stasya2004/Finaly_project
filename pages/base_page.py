218f7b81addd07a03872e29401fc5fa18ede0fad
Anastasis <nastyaparfenova.2004@mail.ru>
2023-04-11 10:31:13 +0300

    Update project

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,10 @@
 
 
-Class BasePage():
+class BasePage():
   def __init__(self, browser, url):
     self.browser = browser
     self.url = url
     
-   def open(self):
+  def open(self):
     self.browser.get(self.url)
     
