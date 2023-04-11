31a23889e16725e999bd995f82c8e339e50f4f3d
Anastasis <nastyaparfenova.2004@mail.ru>
2023-04-11 11:37:02 +0300

    Transitions between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,8 +2,7 @@ from .base_page import BasePage
 
 
 class LoginPage(BasePage):
-    
-    def should_be_login_page(self) -> None:
+    def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
@@ -13,14 +12,21 @@ class LoginPage(BasePage):
         current_url = self.browser.current_url
         assert "login" in current_url, f"No login substring in the {current_url}"
 
-    def should_be_login_form(self) -> None:
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(
-            **.LOGIN_FORM
-                 ), f"Login form is not present on the {self.browser.current_url}"
+    def should_be_login_form(self):
+      # реализуйте проверку, что есть форма логина
+      assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_EMAIL), "Login input email is not presented"
+      assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_PASSWORD), "Login input password is not presented"
+      assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON_LOGIN), "Login button is not presented"
 
-    def should_be_register_form(self) -> None:
+    def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(
-            **.REGISTER_FORM
-                ), f"Register form is not present on the {self.browser.current_url} page"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_EMAIL), "Register input email is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD), "Register input password is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD_CONFIRM), "Register input password is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON_REGISTER), "Register button is not presented"
+
+#    def register_new_user(self, email, password):
+#        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_EMAIL).send_keys(email)
+#        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_PASSWORD).send_keys(password)
+#        self.browser.find_element(*LoginPageLocators.REGISTER_INPUT_PASSWORD_CONFIRM).send_keys(password)
+#        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON_REGISTER).click()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,27 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
   def go_to_login_page(self):
       login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
       login_link.click()
+      alert = self.browser.switch_to.alert
+      alert.accept()
 
   def should_be_login_link(self):
     assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
 
+  def go_to_login_page(self):
+    link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+    link.click()
+    return LoginPage(browser=self.browser, url=self.browser.current_url)   
+
+  def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+    
