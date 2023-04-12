fe8d54b6ef8c83a401ce4abd053923c6a664c801
Anastasis <nastyaparfenova.2004@mail.ru>
2023-04-12 15:51:47 +0300

    adding to the cart from the product page

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class ProductPageLocators:
+    BUTTON_ADD_TO_CART = (By.CLASS_NAME, "btn-add-to-basket")
+    ADDING_SUCCESS = (By.CSS_SELECTOR, "div.alert-success")
+    ALERT_ADDED_TO_CART = (By.CSS_SELECTOR,
+                           "#messages>div:first-child .alertinner strong")
+    ALERT_CART_STATUS = (By.CSS_SELECTOR, ".alert-noicon.alert-info p")
+    PRICE_VALUE = (By.CLASS_NAME, "price_color")
+    PRODUCT_NAME = (By.TAG_NAME, "h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")    
--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,9 @@
 import pytest
 from pages.product_page import ProductPage
 
-link =  "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-
-
 class TestUserAddToCartFromProductPage:
-    @pytest.mark.need_review
-    @pytest.mark.parametrize(link)
-    def test_user_can_add_product_to_cart(self, browser, link: str):
+    def test_user_can_add_product_to_cart(self, browser):
+        link =  "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
         product_page = ProductPage(browser, link) # Страница продукта
         product_page.open()                       # Открываем страницу
         product_page.add_to_cart(True)            # Добавить в корзину
