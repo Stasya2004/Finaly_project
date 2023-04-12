7497d6c76e79d98802898044891677a5459c9ffb
Anastasis <nastyaparfenova.2004@mail.ru>
2023-04-12 16:08:19 +0300

    content independence, looking for a bug

--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,18 @@ import pytest
 from pages.product_page import ProductPage
 
 class TestUserAddToCartFromProductPage:
-    def test_user_can_add_product_to_cart(self, browser):
-        link =  "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+                                           "?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+    def test_guest_can_add_product_to_basket(self, browser, link):
         product_page = ProductPage(browser, link) # Страница продукта
         product_page.open()                       # Открываем страницу
         product_page.add_to_cart(True)            # Добавить в корзину
